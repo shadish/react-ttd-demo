a0269c6b2f0c0820f3fb5dcc3d4e22f2ebe0ced2
shadish <shadtisch@gmail.com>
2018-06-01 23:35:09 -0600

    add redux and some basic styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import CollapseSection from './components/CollapseSection';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <div className="app-header">
-          <h1 className="app-title">Welcome to React</h1>
+          <h1 className="app-title">Lorem Ipsum Dolor</h1>
         </div>
         <div className="app-content">
           <div className="app-sideNav">
-            <div className="app-sideNav-header"> Some header </div>
+            <div className="app-sideNav-header">Some header</div>
             <a className="app-sideNav-element" href="#someLink">Some Link</a>
             <a className="app-sideNav-element" href="#someLink">Some Link</a>
             <a className="app-sideNav-element" href="#someLink">Some Link</a>
+          </div>
+          <div className="app-body">
+            <div className="app-body-header">
+            </div>
+            <CollapseSection />
           </div>
         </div>
       </div>
